ab282c940257a136f432c3d5b8c95491f5a8b78f
navasg <genenavas97@gmail.com>
2020-02-12 11:50:16 -0400

    Slider top

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -28,11 +28,32 @@ $(document).ready(function () {
   });
 });
 
+//invocamos al objeto (window) y a su método (scroll), solo se ejecutara si el usuario hace scroll en la página
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 300) { //condición a cumplirse cuando el usuario aya bajado 301px a más.
+    $("#js_up").slideDown(300); //se muestra el botón en 300 mili segundos
+  } else { // si no
+    $("#js_up").slideUp(300); //se oculta el botón en 300 mili segundos
+  }
+});
+
+//creamos una función accediendo a la etiqueta i en su evento click
+$("#js_up i").on('click', function (e) {
+  e.preventDefault(); //evita que se ejecute el tag ancla (<a href="#">valor</a>).
+  $("body,html").animate({ // aplicamos la función animate a los tags body y html
+    scrollTop: 0 //al colocar el valor 0 a scrollTop me volverá a la parte inicial de la página
+  }, 700); //el valor 700 indica que lo ara en 700 mili segundos
+  return false; //rompe el bucle
+});
+
+
 
 // Menú fixed
 $(window).scroll(function () {
   if ($(document).scrollTop() > 70 && ($(window).width() >= 768)) {
     $('.navbar-fixed-js').addClass('fixed');
+    $('.navbar-fixed-js--custom').addClass('fixed--white');
+    $('.navbar-fixed-js--custom .nav-item a').removeClass('fixed--link');
     $('.nav-link').addClass('fixed-color');
     $('.nav-top__header').addClass('nav-top__header--detele');
     $('.main-brand__fixed').css('display', 'initial');
@@ -44,7 +65,9 @@ $(window).scroll(function () {
     $('nav > div > div.navbar-collapse.offcanvas-collapse > ul > li:nth-child(8) > a').removeClass('contact')
     $('.main-brand__top').css('display', 'initial')
     $('.main-brand__fixed').css('display', 'none')
+    $('.navbar-fixed-js--custom .nav-item a').addClass('fixed--link');
     $('.navbar-fixed-js').removeClass('fixed');
+    $('.navbar-fixed-js--custom').addClass('fixed--white');
     $('.nav-link').removeClass('fixed-color');
     $('.nav-top__header').removeClass('nav-top__header--detele');
     // $("#iso").removeClass('img-size').attr('src', 'assets/img/logo-fvr.jpg').removeClass('scroll-up');
